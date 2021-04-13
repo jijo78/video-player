d917e62de167f8c5188d35daa49122c2bebe09d5
jijo78 <giuche@gmail.com>
2021-04-13 15:28:15 +0100

    Renamed test componennt to Rail.test.js, added mock data and mounted component

---
+++ .history/src/components/Rail/Rails.test_20210413152544.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+
+import Rail from './Rail';
+const mockData = [
+    {
+        id: '3b47f17864b34a2687aabb0f25ea3e3e',
+        title: 'AEK v FC Bayern München',
+        isPromo: false
+    },
+    {
+        id: '91fded81c9fe4e73aeced7b821039ad4',
+        title: 'NFL Network',
+        isPromo: false
+    },
+    {
+        id: '294a3ebc66ec4a1883542468d325f151',
+        title: 'World Series - Dodgers @ Red Sox',
+        isPromo: false
+    }
+];
+describe('<Rail />', function() {
+    const component = mount(<Rail tiles={mockData} />);
+
+});
--- src/components/Rail/Rails.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { expect } from 'chai';
-import sinon from 'sinon';
-import { mount } from 'enzyme';
-
-import Rail from './Rail';
-
-describe('<Rail />', function() {});
